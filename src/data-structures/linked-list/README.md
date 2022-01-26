670489fc5e29d6d136b238a6dd5134ff4d2f3ef5
shushuTona <42406967+shushuTona@users.noreply.github.com>
2022-01-26 05:21:06 +0000

    create LinkedListNode class test code & memo

---
+++ src/data-structures/linked-list/README.md
@@ -0,0 +1,11 @@
+https://github.com/trekhleb/javascript-algorithms/tree/master/src/data-structures/linked-list
+
+## memo
+
+### LinkedListNode
+
+- `value`でそのNodeの値を設定、`next`で次のNodeへの参照を設定する
+- 下記の各`value`は、結局同じvalueのことを指している為、型が一致している必要がある
+    - `value`プロパティ
+    - `next`プロパティに設定するLinkedListNodeの引数のvalue
+    - `toString`メソッドのcallback関数の引数value
---
+++ src/data-structures/linked-list/__test__/LinkedListNode.test.ts
@@ -0,0 +1,55 @@
+import { LinkedListNode, toStringCallbackType } from '../LinkedListNode';
+
+describe('LinkedListNode Test', () => {
+    it('create node', () => {
+        const newNode = new LinkedListNode<number>(1);
+
+        expect(newNode.value).toBe(1);
+        expect(newNode.next).toBeNull();
+    });
+
+    it('create node with obj', () => {
+        const obj = {
+            value: 100,
+            key: 'test',
+        };
+        const newNode = new LinkedListNode<typeof obj>(obj);
+
+        expect(newNode.value.value).toBe(100);
+        expect(newNode.value.key).toBe('test');
+        expect(newNode.next).toBeNull();
+    });
+
+    it('set next node', () => {
+        const newNode2 = new LinkedListNode<number>(2);
+        const newNode1 = new LinkedListNode<number>(1, newNode2);
+
+        // newNode1
+        expect(newNode1.next).toBeDefined();
+        expect(newNode1.next).toBeInstanceOf(LinkedListNode);
+        expect(newNode1.next?.value).toBe(2);
+        expect(newNode1.next?.next).toBeNull();
+
+        // newNode2
+        expect(newNode2.value).toBe(2);
+        expect(newNode2.next).toBeNull();
+    });
+
+    it('execute toString method', () => {
+        const newNode = new LinkedListNode<number>(1);
+
+        expect(newNode.toString()).toBe('1');
+        
+        newNode.value = 100;
+        expect(newNode.toString()).toBe('100');
+    });
+
+    it('execute toString method with callback function', () => {
+        const newNode = new LinkedListNode<number>(1);
+        const callbackFunction: toStringCallbackType<number> = (value) => {
+            return value + '!';
+        };
+
+        expect(newNode.toString(callbackFunction)).toBe('1!');
+    });
+});
