71a1accabed74f5ffe6369aea46821bc107b30e4
shushuTona <42406967+shushuTona@users.noreply.github.com>
2022-01-26 05:20:15 +0000

    fix type definition of LinkedListNode Class

--- src/data-structures/linked-list/LinkedListNode.ts
+++ src/data-structures/linked-list/LinkedListNode.ts
@@ -1,10 +1,15 @@
-export class LinkedListNode {
-    constructor(value, next = null) {
+export type toStringCallbackType<T> = (value: T) => string;
+
+export class LinkedListNode<T> {
+    public value: NonNullable<T>;
+    public next: LinkedListNode<NonNullable<T>>|null;
+
+    constructor(value: NonNullable<T>, next: LinkedListNode<NonNullable<T>>|null = null) {
         this.value = value;
         this.next = next;
     }
 
-    toString(callback) {
+    toString(callback: toStringCallbackType<NonNullable<T>>|null = null) {
         return callback ? callback(this.value) : `${this.value}`;
     }
 }
